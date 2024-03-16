b912e0f95a1596abb671fc0d08f3cc899ee48f9b
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2024-03-17 00:51:24 +0530

    Films

--- src/app/(root)/filmography/page.js
+++ src/app/(root)/filmography/page.js
@@ -15,6 +15,60 @@ export default function Filmography() {
           </Description>
         </Header>
       </HeaderWrapper>
+      <FilmsWrapper>
+        <Film>
+          <FilmImage></FilmImage>
+          <FilmHeading>
+            A Brief Case - A Hindi, Thriller Short Film | some secrets can kill
+          </FilmHeading>
+          <FilmDescription>
+            While the city is sleeping, Senior Inspector Deshmukh is secretly
+            out in search of a briefcase. But he cannot accomplish his secret
+            mission without the help of a lock decoding expert Inspector Sadhu.
+            Being loyal to his duty as a cop will Sadhu help Deshmukh in his
+            dark intended mission? And what is so important in that briefcase?
+          </FilmDescription>
+        </Film>
+        <Film>
+          <FilmImage></FilmImage>
+          <FilmHeading>
+            A Brief Case - A Hindi, Thriller Short Film | some secrets can kill
+          </FilmHeading>
+          <FilmDescription>
+            While the city is sleeping, Senior Inspector Deshmukh is secretly
+            out in search of a briefcase. But he cannot accomplish his secret
+            mission without the help of a lock decoding expert Inspector Sadhu.
+            Being loyal to his duty as a cop will Sadhu help Deshmukh in his
+            dark intended mission? And what is so important in that briefcase?
+          </FilmDescription>
+        </Film>
+        <Film>
+          <FilmImage></FilmImage>
+          <FilmHeading>
+            A Brief Case - A Hindi, Thriller Short Film | some secrets can kill
+          </FilmHeading>
+          <FilmDescription>
+            While the city is sleeping, Senior Inspector Deshmukh is secretly
+            out in search of a briefcase. But he cannot accomplish his secret
+            mission without the help of a lock decoding expert Inspector Sadhu.
+            Being loyal to his duty as a cop will Sadhu help Deshmukh in his
+            dark intended mission? And what is so important in that briefcase?
+          </FilmDescription>
+        </Film>
+        <Film>
+          <FilmImage></FilmImage>
+          <FilmHeading>
+            A Brief Case - A Hindi, Thriller Short Film | some secrets can kill
+          </FilmHeading>
+          <FilmDescription>
+            While the city is sleeping, Senior Inspector Deshmukh is secretly
+            out in search of a briefcase. But he cannot accomplish his secret
+            mission without the help of a lock decoding expert Inspector Sadhu.
+            Being loyal to his duty as a cop will Sadhu help Deshmukh in his
+            dark intended mission? And what is so important in that briefcase?
+          </FilmDescription>
+        </Film>
+      </FilmsWrapper>
     </BaseLayout>
   );
 }
@@ -48,4 +102,51 @@ const Description = styled.p`
   font-size: 20px;
   color: white;
   transition: all 0.5s ease-in-out;
+`;
+
+const FilmsWrapper = styled.div`
+  width: 850px;
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  padding: 50px 0;
+  gap: 3rem;
+`;
+
+const Film = styled.div`
+  width: 400px;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 0 0 10px;
+  border: 2px solid grey;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.5);
+  border-radius: 15px;
+`;
+
+const FilmImage = styled.div`
+  width: 100%;
+  height: 235px;
+  overflow: hidden;
+  border-radius: 15px;
+  background-color: grey;
+`;
+
+const FilmHeading = styled.p`
+  color: white;
+  padding: 0 10px;
+  display: -webkit-box;
+  overflow: hidden;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+`;
+
+const FilmDescription = styled.p`
+  color: lightgray;
+  padding: 0 10px;
+  display: -webkit-box;
+  overflow: hidden;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
 `;
